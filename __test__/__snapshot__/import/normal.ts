146e0d644896b96dafd71e1f81801aa3941b1b8e
TyrealHu <tyrealhu.fe@gmail.com>
2022-12-12 23:09:21 +0800

    feat(tyreal): finish import normal test

--- __test__/__snapshot__/import/normal.ts
+++ __test__/__snapshot__/import/normal.ts
@@ -50,3 +50,323 @@ export const ImportWithoutSpecifiers = {
   ],
   'sourceType': 'module'
 }
+
+export const ImportDefaultSpecifier = {
+  'type': 'Program',
+  'start': 0,
+  'end': 29,
+  'loc': {
+    'start': {
+      'line': 1,
+      'column': 0
+    },
+    'end': {
+      'line': 1,
+      'column': 29
+    }
+  },
+  'body': [
+    {
+      'type': 'ImportDeclaration',
+      'start': 0,
+      'end': 29,
+      'loc': {
+        'start': {
+          'line': 1,
+          'column': 0
+        },
+        'end': {
+          'line': 1,
+          'column': 29
+        }
+      },
+      'importKind': 'value',
+      'specifiers': [
+        {
+          'type': 'ImportDefaultSpecifier',
+          'start': 7,
+          'end': 11,
+          'loc': {
+            'start': {
+              'line': 1,
+              'column': 7
+            },
+            'end': {
+              'line': 1,
+              'column': 16
+            }
+          },
+          'local': {
+            'type': 'Identifier',
+            'start': 7,
+            'end': 11,
+            'loc': {
+              'start': {
+                'line': 1,
+                'column': 7
+              },
+              'end': {
+                'line': 1,
+                'column': 16
+              }
+            },
+            'name': 'test'
+          }
+        }
+      ],
+      'source': {
+        'type': 'Literal',
+        'start': 17,
+        'end': 29,
+        'loc': {
+          'start': {
+            'line': 1,
+            'column': 17
+          },
+          'end': {
+            'line': 1,
+            'column': 29
+          }
+        },
+        'value': './index.js',
+        'raw': '\'./index.js\''
+      }
+    }
+  ],
+  'sourceType': 'module'
+}
+
+export const ImportNameSpecifiers = {
+  'type': 'Program',
+  'start': 0,
+  'end': 39,
+  'loc': {
+    'start': {
+      'line': 1,
+      'column': 0
+    },
+    'end': {
+      'line': 1,
+      'column': 39
+    }
+  },
+  'body': [
+    {
+      'type': 'ImportDeclaration',
+      'start': 0,
+      'end': 39,
+      'loc': {
+        'start': {
+          'line': 1,
+          'column': 0
+        },
+        'end': {
+          'line': 1,
+          'column': 39
+        }
+      },
+      'importKind': 'value',
+      'specifiers': [
+        {
+          'type': 'ImportSpecifier',
+          'start': 9,
+          'end': 13,
+          'loc': {
+            'start': {
+              'line': 1,
+              'column': 9
+            },
+            'end': {
+              'line': 1,
+              'column': 13
+            }
+          },
+          'imported': {
+            'type': 'Identifier',
+            'start': 9,
+            'end': 13,
+            'loc': {
+              'start': {
+                'line': 1,
+                'column': 9
+              },
+              'end': {
+                'line': 1,
+                'column': 13
+              }
+            },
+            'name': 'test'
+          },
+          'importKind': 'value',
+          'local': {
+            'type': 'Identifier',
+            'start': 9,
+            'end': 13,
+            'loc': {
+              'start': {
+                'line': 1,
+                'column': 9
+              },
+              'end': {
+                'line': 1,
+                'column': 13
+              }
+            },
+            'name': 'test'
+          }
+        },
+        {
+          'type': 'ImportSpecifier',
+          'start': 15,
+          'end': 19,
+          'loc': {
+            'start': {
+              'line': 1,
+              'column': 15
+            },
+            'end': {
+              'line': 1,
+              'column': 19
+            }
+          },
+          'imported': {
+            'type': 'Identifier',
+            'start': 15,
+            'end': 19,
+            'loc': {
+              'start': {
+                'line': 1,
+                'column': 15
+              },
+              'end': {
+                'line': 1,
+                'column': 19
+              }
+            },
+            'name': 'name'
+          },
+          'importKind': 'value',
+          'local': {
+            'type': 'Identifier',
+            'start': 15,
+            'end': 19,
+            'loc': {
+              'start': {
+                'line': 1,
+                'column': 15
+              },
+              'end': {
+                'line': 1,
+                'column': 19
+              }
+            },
+            'name': 'name'
+          }
+        }
+      ],
+      'source': {
+        'type': 'Literal',
+        'start': 27,
+        'end': 39,
+        'loc': {
+          'start': {
+            'line': 1,
+            'column': 27
+          },
+          'end': {
+            'line': 1,
+            'column': 39
+          }
+        },
+        'value': './index.js',
+        'raw': '\'./index.js\''
+      }
+    }
+  ],
+  'sourceType': 'module'
+}
+
+export const ImportNamespaceSpecifier = {
+  'type': 'Program',
+  'start': 0,
+  'end': 34,
+  'loc': {
+    'start': {
+      'line': 1,
+      'column': 0
+    },
+    'end': {
+      'line': 1,
+      'column': 34
+    }
+  },
+  'body': [
+    {
+      'type': 'ImportDeclaration',
+      'start': 0,
+      'end': 34,
+      'loc': {
+        'start': {
+          'line': 1,
+          'column': 0
+        },
+        'end': {
+          'line': 1,
+          'column': 34
+        }
+      },
+      'importKind': 'value',
+      'specifiers': [
+        {
+          'type': 'ImportNamespaceSpecifier',
+          'start': 7,
+          'end': 16,
+          'loc': {
+            'start': {
+              'line': 1,
+              'column': 7
+            },
+            'end': {
+              'line': 1,
+              'column': 16
+            }
+          },
+          'local': {
+            'type': 'Identifier',
+            'start': 12,
+            'end': 16,
+            'loc': {
+              'start': {
+                'line': 1,
+                'column': 12
+              },
+              'end': {
+                'line': 1,
+                'column': 16
+              }
+            },
+            'name': 'test'
+          }
+        }
+      ],
+      'source': {
+        'type': 'Literal',
+        'start': 22,
+        'end': 34,
+        'loc': {
+          'start': {
+            'line': 1,
+            'column': 22
+          },
+          'end': {
+            'line': 1,
+            'column': 34
+          }
+        },
+        'value': './index.js',
+        'raw': '\'./index.js\''
+      }
+    }
+  ],
+  'sourceType': 'module'
+}
--- __test__/import/normal.test.ts
+++ __test__/import/normal.test.ts
@@ -1,10 +1,29 @@
 import { parseSource } from '../utils'
-import { ImportWithoutSpecifiers } from '../__snapshot__/import/normal'
+import {
+  ImportWithoutSpecifiers,
+  ImportDefaultSpecifier,
+  ImportNameSpecifiers,
+  ImportNamespaceSpecifier
+} from '../__snapshot__/import/normal'
 
 describe('normal syntax', () => {
   it('import without specifiers', () => {
     const node = parseSource(`import './index.js'`)
-    // console.log(JSON.stringify(node, null, 2))
     expect(node).toEqual(ImportWithoutSpecifiers)
+  })
+
+  it('import default specifiers', () => {
+    const node = parseSource(`import test from './index.js'`)
+    expect(node).toEqual(ImportDefaultSpecifier)
+  })
+
+  it('import name specifiers', function() {
+    const node = parseSource(`import { test, name } from './index.js'`)
+    expect(node).toEqual(ImportNameSpecifiers)
+  })
+
+  it('import namespace specifiers', function() {
+    const node = parseSource(`import * as test from './index.js'`)
+    expect(node).toEqual(ImportNamespaceSpecifier)
   })
 })
